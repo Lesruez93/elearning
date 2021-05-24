546af01296cacc9fe721d15735bc52abe2ea5b56
Lesruez93 <lesterrusike@gmail.com>
2021-05-24 11:42:35 +0200

    new-app

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -10,6 +10,10 @@ import {GetterSetterService} from '../getter-setter.service';
 })
 export class SearchPage implements OnInit {
   data: any = {};
+  filter: boolean;
+  region: any = 'windhoek';
+  searching:boolean;
+  regions: any;
 
   constructor(
       private navCtrl:NavController,
@@ -20,36 +24,47 @@ export class SearchPage implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.http.get('https://portal.audioguide-namibia.com/categories/').subscribe((res:any)=>{
+      this.regions = res.categories
+    },error1 => {})
   }
 
-    clear() {
 
-    }
 
   al() {
     this.navCtrl.navigateRoot('/')
   }
 
-  change($event: CustomEvent<any>) {
 
-  }
 
-  async search(value: any) {
-    console.log(value)
+    async search(value: any) {
 
- let loader = await this.loadingCtrL.create({
-  message:'Searching',
+      this.searching = true;
+  setTimeout(()=>{
 
-})
-await loader.present()
+    this.filter = false;
+  },1000);
+//  let loader = await this.loadingCtrL.create({
+//   message:'Searching',
+//
+// })
+// await loader.present()
+
     this.http.get('https://portal.audioguide-namibia.com/search/'+value).subscribe((res:any)=>{
-      console.log(res)
-      this.data = res
+      console.log(res);
+      this.data = res;
+      if (res.items.length == 0  || res.items.length == undefined)
+      {
+        alert('No results were found try again!')
+      }
 
-      loader.dismiss()
-      console.log(res)
+      //loader.dismiss()
+      this.searching = false;
     },error1 => {
-      loader.dismiss()
+     // loader.dismiss()
+      this.searching = false;
+      alert('Something went wrong:!')
+
     })
   }
 
